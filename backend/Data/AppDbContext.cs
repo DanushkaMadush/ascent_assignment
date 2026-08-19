ee2ed4991091f8f7e3a4f34203faab2784be5cdc
danushka <danmadushzxc@gmail.com>
2026-08-19 13:09:59 +0530

    feat:added configurations

---
+++ backend/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using backend.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees => Set<Employee>();
+
+        public DbSet<Department> Departments => Set<Department>();
+
+        public DbSet<Attendance> Attendances => Set<Attendance>();
+
+        public DbSet<Leave> Leaves => Set<Leave>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(
+                typeof(AppDbContext).Assembly);
+        }
+    }
+}
---
+++ backend/Data/Configurations/ApplicationUserConfiguration.cs
@@ -0,0 +1,6 @@
+﻿namespace backend.Data.Configurations
+{
+    public class ApplicationUserConfiguration
+    {
+    }
+}
