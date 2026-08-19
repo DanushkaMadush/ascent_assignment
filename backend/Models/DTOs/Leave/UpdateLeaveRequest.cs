b3d5fedaf0ed59c2ad21e3f23039ab91269835a0
danushka <danmadushzxc@gmail.com>
2026-08-19 23:19:48 +0530

    feat:implemented leave service

---
+++ backend/Models/DTOs/Leave/UpdateLeaveRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models.DTOs.Leave
+{
+    public class UpdateLeaveRequest
+    {
+        [Required]
+        public string LeaveType { get; set; } = null!;
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [MaxLength(1000)]
+        public string? Reason { get; set; }
+    }
+}
