262b83ccf333f7d2023e334e378aab57075fd67f
danushka <danmadushzxc@gmail.com>
2026-08-19 14:26:36 +0530

    feat:added common responses

---
+++ backend/Common/PagedResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace backend.Common
+{
+    public class PagedResponse<T>
+    {
+        public IReadOnlyList<T> Data { get; set; } = [];
+
+        public PaginationMetadata Pagination { get; set; } = new();
+    }
+
+    public class PaginationMetadata
+    {
+        public int PageNumber { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int TotalRecords { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public bool HasPrevious { get; set; }
+
+        public bool HasNext { get; set; }
+    }
+}
