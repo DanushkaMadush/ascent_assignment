e17f02e6f98de58faa1ec689f3c772987b9af88e
danushka <danmadushzxc@gmail.com>
2026-08-20 17:06:26 +0530

    refactor:added home route

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { AuthGuard } from "./AuthGuard";
 import LoginPage from "../pages/auth/LoginPage";
 import UnauthorizedPage from "../pages/auth/UnauthorizedPage";
@@ -7,14 +7,17 @@ import EmployeePage from "../pages/employee/EmployeePage";
 import DepartmentPage from "../pages/department/DepartmentPage";
 import AttendancePage from "../pages/attendance/AttendancePage";
 import LeavePage from "../pages/leave/LeavePage";
+import HomePage from "../pages/home/HomePage";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         
+        <Route path="/" element={<Navigate to="/login" replace/>} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/unauthorized" element={<UnauthorizedPage />} />
+        <Route path="/home" element={<HomePage />} />
 
         <Route element={<AuthGuard allowedRoles={["Employee", "Manager"]} />}>
           <Route path="/attendance" element={<AttendancePage />} />
